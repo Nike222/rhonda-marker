f2c8bf8b4cff3edce8bd41ee72668ec8ddf8438a
Roman Orlov <orlov@farpost.com>
2011-11-29 23:54:45 +1100

    application веб словаря

---
+++ application/config/application.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Path to libraries directory
+ *
+ */
+$config['xml_libraries'] = FCPATH.'var/libraries/';
+
+/**
+ * Path to xml-schema file
+ *
+ */
+$config['xml_schema'] = FCPATH.'var/schema/library.xsd';
+
+?>
